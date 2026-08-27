1f902469a7bb36574f36d4304f2fecaf218df57e
joachim-reuter <joreu1964@gmail.com>
2026-08-27 19:49:10 +0200

    Add User_Setup.h for TFT display configuration

---
+++ User_Setup.h
@@ -0,0 +1,26 @@
+
+#define ILI9341_DRIVER
+
+#define TFT_WIDTH  320
+#define TFT_HEIGHT 240
+
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   15
+#define TFT_DC   2
+#define TFT_RST  4
+
+#define TOUCH_CS 21
+
+#define SPI_FREQUENCY       40000000
+#define SPI_TOUCH_FREQUENCY 2500000
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
